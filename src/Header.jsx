47e2af72968fbf875e2add716b196ec1b98834bf
Nikunj-Mehta <mehtanikunj11@gmail.com>
2025-02-05 21:05:25 +0530

    made some changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,9 @@
-// import chefClaudeLogo from "C:/Users/NIKUNJ MEHTA/Desktop/codes/ReactProjectsDeployed/chefClaude/src/images/chef-claude-icon.png"
+import chefClaudeLogo from "./images/chef-claude-icon.png"
 
 export default function Header() {
     return (
         <header>
-            {/* <img src={chefClaudeLogo}/> */}
+            <img src={chefClaudeLogo}/>
             <h1>Chef Claude</h1>
         </header>
     )
